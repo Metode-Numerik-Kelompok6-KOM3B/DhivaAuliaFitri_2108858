108d66f672f69651abece4938f5d7d3f5267e657
dhivaaf <119849232+dhivaaf@users.noreply.github.com>
2023-01-05 09:59:24 +0700

    Create METODE_LAGRANGE.py

---
+++ METODE_LAGRANGE.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+n = int(input('Masukkan jumlah titik: '))
+nilaiX = np.zeros((n))
+nilaiY = np.zeros((n))
+
+print('Masukan data X dan Y: ')
+for i in range(n):
+    nilaiX[i] = float(input( 'nilaiX['+str(i)+']='))
+    nilaiY[i] = float(input( 'nilaiY['+str(i)+']='))
+    
+#===========Mendeteksi titik Interpolasi=============#
+nilaiXP = float(input('Masukkan titik interpolasi: '))
+nilaiYP = 0
+
+#====================Solusi==========================#
+for i in range(n):
+    p = 1
+    for j in range(n):
+        if i != j:
+            p = p * (nilaiXP - nilaiX[j])/(nilaiX[i] - nilaiX[j])
+    nilaiYP = nilaiYP + p * nilaiY[i]    
+print('Nilai interpolasi untuk titik %.3f adalah %.3f.' % (nilaiXP, nilaiYP))
+
+
