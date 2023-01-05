e4b9cf04e1383d050d692ab7b00fc828ab5924a2
dhivaaf <119849232+dhivaaf@users.noreply.github.com>
2023-01-05 10:32:26 +0700

    Update METODE_INTEGRAL_SIMSON1PER3.py

--- METODE_INTEGRAL_SIMSON1PER3.py
+++ METODE_INTEGRAL_SIMSON1PER3.py
@@ -1,26 +1,27 @@
+#===========Mendefinisikan Fungsi=============#
 e= 2.71828                  
 def f(x):
     z = eval(fx)            
     return z             
 
-# Algoritma Simpson1/3
 def simpson13(a,b,n):
     h = (b - a) / n                 
-    integration = f(a) + f(b)        #menjumlahkan fungsi dari batas bawah dan batas bawah
+    integration = f(a) + f(b)                   #menjumlahkan fungsi dari batas bawah dan batas bawah
     for i in range(1,n): 
-        k = a + i*h                  # menentukan titik selanjutnya 
-        if i%2 == 0:                 # jika i genap, maka f(k) dikali 2
+        k = a + i*h                        
+        if i%2 == 0:                            #menentukan titik denfan syarat, jika i genap maka f(k) dikali 2
             integration = integration + 2 * f(k)
-        elif i%2 == 1:               # jika i ganjil, maka f(k) dikali 4
+        elif i%2 == 1:                          #dan jika i ganjil, maka f(k) dikali 4
             integration = integration + 4 * f(k)
-        
     integration = integration * h/3
-    return integration
-    
+    return integration                          #Mengembalikan nilai penjumlahan
+
+#===============Masukan Inputan=================#
 fx = str(input("Masukkan fungsi integral : "))
 a = float(input("Masukkan batas bawah: "))
 b = float(input("Masukkan batas atas: "))
 n = int(input("Masukkan jumlah iterasi: "))
 
+#===============Menampilkan result=================#
 result = simpson13(a, b, n)
 print("Integral dengan metode trapesium: %0.6f" % (result))
