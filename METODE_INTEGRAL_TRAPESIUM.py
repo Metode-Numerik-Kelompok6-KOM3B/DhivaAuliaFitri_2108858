7994ecbb30b7184755d3a97524ce311a295db4cf
dhivaaf <119849232+dhivaaf@users.noreply.github.com>
2023-01-05 10:28:24 +0700

    Update METODE_INTEGRAL_TRAPESIUM.py

--- METODE_INTEGRAL_TRAPESIUM.py
+++ METODE_INTEGRAL_TRAPESIUM.py
@@ -1,21 +1,24 @@
+#===========Definisikan Fungsi=============#
 e= 2.71828                 
 def f(x):
-    z = eval(fx)            
-    return z               
+    z = eval(fx)                                #eval berfungsi untuk mengoperasikan fungsi dari bentuk string           
+    return z                                    #Me return nilai Z
 
 def trapezoidal(a,b,n):
-    h = (b - a) / n                  
-    integration = f(a) + f(b)       
+    h = (b - a) / n                                 
+    integration = f(a) + f(b)                   #Meintegration fungsi a dan fungsi b      
     for i in range(1,n):            
         k = a + i*h                 
         integration = integration + 2 * f(k)   
     integration = integration * h/2
     return integration            
     
+#===========Memasukan Inputan=============#
 fx = str(input("Masukkan fungsi integral : "))
 a = float(input("Masukkan batas bawah: "))
 b = float(input("Masukkan batas atas: "))
 n = int(input("Masukkan jumlah iterasi: "))
 
+#===========Menampilkan Result=============#
 result = trapezoidal(a, b, n)
 print("Integral dengan metode trapesium: %0.6f" % (result))
